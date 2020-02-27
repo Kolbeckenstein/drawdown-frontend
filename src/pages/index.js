7aef78991ccd138a6c7a7e072dc813972e3172cd
Michael Kolbeck <michael.b.kolbeck@gmail.com>
2020-02-27 12:07:31 -0500

    Fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,10 +12,6 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <p>This is some additional content</p>
-<<<<<<< HEAD
-    <p>This should also appear if automation works.</p>
-    <p>Some data:</p>
-=======
     <p>This should also appear if deploy automation works.</p>
     <p>If integration works, data should appear below: </p>
     {data.allStrapiSolution.edges.map(document => (
@@ -24,7 +20,6 @@ const IndexPage = () => (
         <p>{document.node.Description}</p>
       </div>
     ))}
->>>>>>> f69b12f78de48fbb2e710f2833244b27e99a78e3
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
@@ -32,17 +27,6 @@ const IndexPage = () => (
   </Layout>
 )
 
-<<<<<<< HEAD
-//     allStrapiSolution {
-//       edges {
-//         node {
-//           Name
-//         }
-//       }
-//     }
-//   }
-// `
-=======
 export default IndexPage
 
 export const pageQuery = graphql`
@@ -58,7 +42,6 @@ export const pageQuery = graphql`
     }
   }
 `
->>>>>>> f69b12f78de48fbb2e710f2833244b27e99a78e3
 
 // export const pageQuery = graphql`
 //   query IndexQuery {
